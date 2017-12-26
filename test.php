8a9a555673f7a72191003dc80335069f73008dae
shayvmo <1006897172@qq.com>
2017-12-26 10:17:19 +0800

    change item.js

---
+++ test.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2017/12/25
+ * Time: 14:39
+ */
+$a = array('2', '3', '4');
+foreach ($a as $key => $val){
+    echo $val,',';
+}
